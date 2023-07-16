9f7686418f8d33ca25fb2ce6706754b6ed74e075
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-07-15 19:22:45 -0600

    Update producto-index.component.ts

--- client/app/src/app/producto/producto-index/producto-index.component.ts
+++ client/app/src/app/producto/producto-index/producto-index.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
+import { ProductoDiagComponent } from '../producto-diag/producto-diag.component';
 
 @Component({
   selector: 'app-producto-index',
@@ -6,5 +10,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./producto-index.component.css']
 })
 export class ProductoIndexComponent {
+  datos:any;
+  destroy$:Subject<boolean>=new Subject<boolean>();
+
+  constructor(private gService:GenericService,
+    private dialog:MatDialog
+    ){
+    this.listaProductos(); 
+  }
+
+  listaProductos(){
+    this.gService.list('producto/')
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data:any)=>{
+    console.log(data);
+    this.datos=data;
+    });
+    }
+
+  detalleProducto(id:number){
+    const dialogConfig=new MatDialogConfig();
+    dialogConfig.disableClose=false;
+    dialogConfig.data={
+    id_producto:id
+    };
+    this.dialog.open(ProductoDiagComponent,dialogConfig);
+  }
 
 }
