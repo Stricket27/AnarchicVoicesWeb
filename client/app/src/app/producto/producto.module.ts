ccde4dbd43057ed815f47d4cd3d0b5b3688b500d
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-07-15 19:37:49 -0600

    Update producto.module.ts

--- client/app/src/app/producto/producto.module.ts
+++ client/app/src/app/producto/producto.module.ts
@@ -8,6 +8,21 @@ import { ProductoDiagComponent } from './producto-diag/producto-diag.component';
 import { ProductoFormComponent } from './producto-form/producto-form.component';
 import { ProductoIndexComponent } from './producto-index/producto-index.component';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatDividerModule} from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';  
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule} from "@angular/material/dialog";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
@@ -15,7 +30,22 @@ import { ProductoIndexComponent } from './producto-index/producto-index.componen
     ProductoDetailComponent,
     ProductoDiagComponent,
     ProductoFormComponent,
-    ProductoIndexComponent
+    ProductoIndexComponent,
+
+     MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
   ],
   imports: [
     CommonModule,
