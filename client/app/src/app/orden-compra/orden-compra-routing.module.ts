0deee694ce6ca568779317af050a278c9c90baf1
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-07-15 19:43:53 -0600

    Update orden-compra-routing.module.ts

--- client/app/src/app/orden-compra/orden-compra-routing.module.ts
+++ client/app/src/app/orden-compra/orden-compra-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { OrdenCompraAllComponent } from './orden-compra-all/orden-compra-all.component';
+import { OrdenCompraDetailComponent } from './orden-compra-detail/orden-compra-detail.component';
+import { OrdenCompravAllComponent } from './orden-comprav-all/orden-comprav-all.component';
+import { OrdenCompravDetailComponent } from './orden-comprav-detail/orden-comprav-detail.component';
+
+const routes: Routes = [
+  {path:'orden-compra/all', component: OrdenCompraAllComponent},
+
+  {path:'orden-compra/:id', component: OrdenCompraDetailComponent},
+  {path:'orden-comprav/all', component: OrdenCompravAllComponent},
+  {path:'orden-comprav/:id', component: OrdenCompravDetailComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
