596c452d803b0df13e2f810f37f66caeb473eb48
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-07-15 19:42:51 -0600

    Update producto-routing.module.ts

--- client/app/src/app/producto/producto-routing.module.ts
+++ client/app/src/app/producto/producto-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ProductoAllComponent } from './producto-all/producto-all.component';
+import { ProductoDetailComponent } from './producto-detail/producto-detail.component';
+import { ProductoFormComponent } from './producto-form/producto-form.component';
+import { ProductoIndexComponent } from './producto-index/producto-index.component';
+
+const routes: Routes = [
+  {path: 'producto', component: ProductoIndexComponent},
+  {path: 'producto/all', component: ProductoAllComponent},
+  {path: 'producto/create', component: ProductoFormComponent},
+  {path: 'producto/:id', component: ProductoDetailComponent},
+  {path: 'producto/update/:id', component: ProductoFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
