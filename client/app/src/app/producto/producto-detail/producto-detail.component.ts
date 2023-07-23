b182bb77a718bc931b6fa951eb0373b21ea595be
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-07-23 10:33:56 -0600

    Update producto-detail.component.ts

--- client/app/src/app/producto/producto-detail/producto-detail.component.ts
+++ client/app/src/app/producto/producto-detail/producto-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { GenericService } from 'src/app/share/generic.service';
 
 @Component({
   selector: 'app-producto-detail',
@@ -6,5 +9,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./producto-detail.component.css']
 })
 export class ProductoDetailComponent {
+  datos:any;
+  destroy$:Subject<boolean>=new Subject<boolean>();
 
+  constructor( private gService: GenericService,
+  private route:ActivatedRoute
+  ){
+  let id=this.route.snapshot.paramMap.get('id');
+  if(!isNaN(Number(id))){
+  this.obtenerProducto(Number(id));
+  }
+  }
+
+  obtenerProducto(id:any){
+  this.gService
+  .get('producto',id)
+  .pipe(takeUntil(this.destroy$))
+  .subscribe((data:any)=>{
+  console.log(data);
+  this.datos=data; 
+  });
+  }
+
+  ngOnDestroy() {
+  this.destroy$.next(true);
+  this.destroy$.unsubscribe();
+  }
 }
