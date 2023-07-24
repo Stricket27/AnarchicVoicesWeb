4cc940ec11b5261dc1711a50f3f1d26c29024e6d
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-07-23 20:00:52 -0600

    Update fotografia-routing.module.ts

--- client/app/src/app/fotografia/fotografia-routing.module.ts
+++ client/app/src/app/fotografia/fotografia-routing.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { FotografiaAllComponent } from './fotografia-all/fotografia-all.component';
-import { FotografiaDetailComponent } from './fotografia-detail/fotografia-detail.component';
 import { FotografiaFormComponent } from './fotografia-form/fotografia-form.component';
 
 const routes: Routes = [
   {path: 'fotografia/all', component: FotografiaAllComponent},
-  {path: 'fotografia/:id', component: FotografiaDetailComponent},
   {path: 'fotografia/create', component: FotografiaFormComponent},
   {path: 'fotografia/update/:id', component: FotografiaFormComponent}
 ];
