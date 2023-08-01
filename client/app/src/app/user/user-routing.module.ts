c3f01c90d044a64b87d0d2402c6926d2f5b1b93c
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-01 13:11:47 -0600

    Update user-routing.module.ts

--- client/app/src/app/user/user-routing.module.ts
+++ client/app/src/app/user/user-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-
+import { UserIndexComponent } from './user-index/user-index.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserCreateComponent } from './user-create/user-create.component';
 
 const routes: Routes = [
-
+  {
+    path: 'user',
+    component: UserIndexComponent,
+    children: [
+      { path: 'registrar', component: UserCreateComponent },
+      { path: 'login', component: UserLoginComponent },
+    ],
+  },
 ];
 
 @NgModule({
