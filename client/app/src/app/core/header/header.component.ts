2ad7e5a428e2a6c442c3724b249ea0c19bbc9fa2
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-01 13:31:22 -0600

    Update header.component.ts

--- client/app/src/app/core/header/header.component.ts
+++ client/app/src/app/core/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/share/cart.service';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/share/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +9,35 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  isAutenticated: boolean;
+  currentUser: any;
+  qtyItems:Number = 0;
+  constructor(private cartService: CartService,
+    private router: Router,
+    private authService: AuthenticationService) {      
+        this.qtyItems=this.cartService.quantityItems()
+  }
 
+  ngOnInit(): void {
+
+
+    //Suscripción a la información del usuario actual
+    this.authService.currentUser.subscribe((x) => (this.currentUser = x))
+    //Suscripción al booleano que indica si esta autenticado
+    this.authService.isAuthenticated.subscribe(
+      (valor) => (this.isAutenticated = valor)
+    )
+
+    //Suscribirse al observable que gestiona la cantidad de items del carrito
+    this.cartService.countItems.subscribe((value)=>{
+      this.qtyItems=value
+     })
+  }
+  login(){
+    this.router.navigate(['user/login'])
+  }
+  logout(){
+    this.authService.logout();
+    this.router.navigate(['inicio'])
+  }
 }
