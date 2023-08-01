79e45a4dd993e728aa092979185b473579f6fb2c
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-01 13:37:44 -0600

    Update user-index.component.ts

--- client/app/src/app/user/user-index/user-index.component.ts
+++ client/app/src/app/user/user-index/user-index.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NotificacionService, TipoMessage } from 'src/app/share/notification.service';
 
 @Component({
   selector: 'app-user-index',
@@ -6,5 +8,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-index.component.css']
 })
 export class UserIndexComponent {
-
+  constructor(
+    private router: Router, 
+    private route: ActivatedRoute) {
+  }
+  ngOnInit(): void {
+    this.router.navigate(['/user/login'], { relativeTo: this.route });
+  }
 }
