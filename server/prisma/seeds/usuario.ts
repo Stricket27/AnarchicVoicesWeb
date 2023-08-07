0a38cfdf7e6626b5cf4d6cdbf6a9ec8c802c3f2f
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-07 13:20:40 -0600

    Update usuario.ts

--- server/prisma/seeds/usuario.ts
+++ server/prisma/seeds/usuario.ts
@@ -4,7 +4,7 @@ export const usuario = [
         apellidos: "Quiros",
         numero_telefono: "85525418", 
         correo_electronico: "mquirosca@est.utn.ac.cr",
-        contrasenna: "ADMIN123",
+        contrasenna: "$2b$10$YzCHIwp7DBycS10bCRPEXe1rk7IG7mPWEWSN9oW17rdcIoYxhNWs6", //ADMIN123
         estado_actual: "Activo"
     },
     {
@@ -12,7 +12,7 @@ export const usuario = [
         apellidos: "Ledezma",
         numero_telefono: "85012459", 
         correo_electronico: "dlednezma123@gmail.com",
-        contrasenna: "Vend123",
+        contrasenna: "$2b$10$KdZdUBcV7reTpkYdsd.9lu3J3un6lfliGMnO1F7h20YpyPysWuLgK", //Vend123
         estado_actual: "Activo"
     },
     {
@@ -20,7 +20,7 @@ export const usuario = [
         apellidos: "Blanco",
         numero_telefono: "85692304", 
         correo_electronico: "yblanco@est.utn.ac.cr",
-        contrasenna: "Client123",
+        contrasenna: "$2b$10$2urApRfvZ3HobgQHw0I/9.b.0yJ9i8/UTzc6gV8Y9D9g0PFlKZHKO", //Client123
         estado_actual: "Activo"
     },
     {
@@ -28,7 +28,7 @@ export const usuario = [
         apellidos: "Rodriguez",
         numero_telefono: "84605270", 
         correo_electronico: "jrodriguez@gmail.com",
-        contrasenna: "Vend456",
+        contrasenna: "$2b$10$/kvdQIkwpiGtEHGe2jjyDuBrOMlv4KaquuHQLeCVU4Jugir2Ho.P6", //Vend456
         estado_actual: "Activo"
     },
     {
@@ -36,7 +36,7 @@ export const usuario = [
         apellidos: "Villegaz",
         numero_telefono: "70856023", 
         correo_electronico: "hvillegas@gmail.com",
-        contrasenna: "VendClient123",
+        contrasenna: "$2b$10$NQd.qRB4slh9t5VhsAmE1ORGE.IVAeOAsx3LqfZgyakT.HBdl.IYS", //VendClient123
         estado_actual: "Activo"
     },
 
