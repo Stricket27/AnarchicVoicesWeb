e49fe8d29fed728722fa44828c1cf62fda3dadfb
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-07 18:53:40 -0600

    Update producto-all.component.ts

--- client/app/src/app/producto/producto-all/producto-all.component.ts
+++ client/app/src/app/producto/producto-all/producto-all.component.ts
@@ -15,79 +15,79 @@ import { GenericService } from 'src/app/share/generic.service';
 export class ProductoAllComponent {
   usuarios: any[];
   usuariosCargados = false;
-  datos:any;
-  destroy$:Subject<boolean>=new Subject<boolean>();
+  datos: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
-  
-  dataSource= new MatTableDataSource<any>();
+
+  dataSource = new MatTableDataSource<any>();
   displayedColumns = ['producto', 'precio', 'cantidad', 'id_usuario', 'acciones'];
 
-  constructor(private router:Router,
-  private route:ActivatedRoute,
-  private gService:GenericService) {
+  constructor(private router: Router,
+    private route: ActivatedRoute,
+    private gService: GenericService) {
   }
 
   ngAfterViewInit(): void {
-  this.listaProductos();
-  this.listaUsuarios();
-  }
-  
-  listaProductos(){
-  this.gService.list('producto/')
-  .pipe(takeUntil(this.destroy$), map((data: any[]) => data.filter(producto => producto.id_usuario == 2)))
-  .subscribe((data:any)=>{
-  console.log(data);
-  this.datos=data;
-  this.dataSource = new MatTableDataSource(this.datos);
-  this.dataSource.sort = this.sort;
-  this.dataSource.paginator = this.paginator;        
-  });   
+    this.listaProductos();
+    this.listaUsuarios();
   }
 
-  listaUsuarios(){
-  this.gService.list('user/')
-  .pipe(takeUntil(this.destroy$))
-  .subscribe((data: any) => {
-  console.log(data);
-  this.usuarios = data;
-  console.log(this.usuarios)
-  this.usuariosCargados = true; 
-  });   
+  listaProductos() {
+    this.gService.list('producto/')
+      .pipe(takeUntil(this.destroy$), map((data: any[]) => data.filter(producto => producto.id_usuario == 2)))
+      .subscribe((data: any) => {
+        console.log(data);
+        this.datos = data;
+        this.dataSource = new MatTableDataSource(this.datos);
+        this.dataSource.sort = this.sort;
+        this.dataSource.paginator = this.paginator;
+      });
+  }
+
+  listaUsuarios() {
+    this.gService.list('user/')
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        console.log(data);
+        this.usuarios = data;
+        console.log(this.usuarios)
+        this.usuariosCargados = true;
+      });
   }
 
   getNombreUsuario(id: number): string {
-  if (this.usuariosCargados) {
-  const usuario = this.usuarios.find(u => u.id_usuario === id);
-  return usuario ? usuario.nombre+" "+usuario.apellidos : '';
-  } else {
-  return '';
-  }
+    if (this.usuariosCargados) {
+      const usuario = this.usuarios.find(u => u.id_usuario === id);
+      return usuario ? usuario.nombre + " " + usuario.apellidos : '';
+    } else {
+      return '';
+    }
   }
 
-  detalle(id:number){
-  this.router.navigate(['/producto',id],
-  {
-  relativeTo:this.route
-  })
+  detalle(id: number) {
+    this.router.navigate(['/producto', id],
+      {
+        relativeTo: this.route
+      })
   }
 
   actualizarProducto(id: number) {
-  this.router.navigate(['/producto/update', id], {
-  relativeTo: this.route,
-  });
+    this.router.navigate(['/producto/update', id], {
+      relativeTo: this.route,
+    });
   }
 
   crearProducto() {
-  this.router.navigate(['/producto/create'], {
-  relativeTo: this.route,
-  });
+    this.router.navigate(['/producto/create'], {
+      relativeTo: this.route,
+    });
   }
 
-  ngOnDestroy(){
-  this.destroy$.next(true);
-  this.destroy$.unsubscribe();
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    this.destroy$.unsubscribe();
   }
 
 }
