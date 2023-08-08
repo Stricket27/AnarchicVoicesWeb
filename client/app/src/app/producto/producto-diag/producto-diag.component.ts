d6bcc3646d308d473c45da3642302ff275b88a28
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-07 18:57:18 -0600

    Update producto-diag.component.ts

--- client/app/src/app/producto/producto-diag/producto-diag.component.ts
+++ client/app/src/app/producto/producto-diag/producto-diag.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Inject,OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { Subject, map, takeUntil } from 'rxjs';
 import { GenericService } from 'src/app/share/generic.service';
@@ -11,37 +11,36 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './producto-diag.component.html',
   styleUrls: ['./producto-diag.component.css']
 })
-export class ProductoDiagComponent implements OnInit{
-  datos:any;
-  datosDialog:any;
-  destroy$:Subject<boolean>= new Subject<boolean>();
+
+export class ProductoDiagComponent implements OnInit {
+  datos: any;
+  datosDialog: any;
+  destroy$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
-  @Inject(MAT_DIALOG_DATA) data,private router:Router,
-  private route:ActivatedRoute,
-  private dialogRef:MatDialogRef<ProductoDiagComponent>,
-  private gService:GenericService,public dialog: MatDialog
-  ) { 
-  this.datosDialog=data;
+    @Inject(MAT_DIALOG_DATA) data, private router: Router,
+    private route: ActivatedRoute,
+    private dialogRef: MatDialogRef<ProductoDiagComponent>,
+    private gService: GenericService, public dialog: MatDialog
+  ) {
+    this.datosDialog = data;
   }
 
   ngOnInit(): void {
-  if(this.datosDialog.id_producto){
-  this.obtenerProducto(this.datosDialog.id_producto);
-  }
-  }
-
-  obtenerProducto(id:any){
-  this.gService
-  .get('producto',id)
-  .pipe(takeUntil(this.destroy$))
-  .subscribe((data:any)=>{
-  this.datos=data;
-  console.log(data);
-  });
+    if (this.datosDialog.id_producto) {
+      this.obtenerProducto(this.datosDialog.id_producto);
+    }
   }
 
-  
+  obtenerProducto(id: any) {
+    this.gService
+      .get('producto', id)
+      .pipe(takeUntil(this.destroy$))
+      .subscribe((data: any) => {
+        this.datos = data;
+        console.log(data);
+      });
+  }
 
   crearMensaje() {
     this.router.navigate(['/mensajeria-cliente/create'], {
@@ -49,9 +48,26 @@ export class ProductoDiagComponent implements OnInit{
       queryParams: { id_producto: this.datosDialog.id_producto }
     });
     this.close();
-    
-    }
-  close(){
-  this.dialogRef.close();
   }
+
+  crearFotografia() {
+    this.router.navigate(['/fotografia/create'], {
+      relativeTo: this.route,
+      queryParams: { id_producto: this.datosDialog.id_producto }
+    });
+    this.close();
+  }
+
+  // editarFotografia(){
+  // this.router.navigate(['/fotografia/'], {
+  // relativeTo: this.route,
+  // queryParams: { id_producto: this.datosDialog.id_producto }
+  // });
+  // this.close();
+  // }
+
+  close() {
+    this.dialogRef.close();
+  }
+
 }
