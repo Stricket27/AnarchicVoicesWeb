17a69bb42dd4e4192df7538fc4783a9958256868
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-07 18:54:46 -0600

    Update user-login.component.ts

--- client/app/src/app/user/user-login/user-login.component.ts
+++ client/app/src/app/user/user-login/user-login.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute, Router } from '@angular/router';
-import { AuthenticationService } from 'src/app/share/authentication.service';
-import { NotificacionService, TipoMessage } from 'src/app/share/notification.service';
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { AuthenticationService } from "src/app/share/authentication.service";
+import {
+  NotificacionService,
+  TipoMessage,
+} from "src/app/share/notification.service";
 
 @Component({
-  selector: 'app-user-login',
-  templateUrl: './user-login.component.html',
-  styleUrls: ['./user-login.component.css']
+  selector: "app-user-login",
+  templateUrl: "./user-login.component.html",
+  styleUrls: ["./user-login.component.css"],
 })
 export class UserLoginComponent {
-  hide=true;
+  hide = true;
   formulario: FormGroup;
   makeSubmit: boolean = false;
   infoUsuario: any;
@@ -26,8 +29,8 @@ export class UserLoginComponent {
   // Definir el formulario con su reglas de validación
   reactiveForm() {
     this.formulario = this.fb.group({
-      correo_electronico: ['', [Validators.required, Validators.email]],
-      contrasenna: ['', Validators.required],
+      correo_electronico: ["", [Validators.required, Validators.email]],
+      contrasenna: ["", Validators.required],
     });
   }
   ngOnInit(): void {
@@ -35,42 +38,44 @@ export class UserLoginComponent {
   }
 
   mensajes() {
-   //Mensaje usuario registrado
-   let register = false;
-   let auth = '';
-   //Obtener parámetros de la URL
-   this.route.queryParams.subscribe((params) =>{
-    register = params['register'] === 'true' || false;
-    auth=params['auth'] || ''
-    if(register){
-      this.notificacion.mensaje(
-        'Usuario',
-        'Uusario registrado! Especifique sus credenciales',
-        TipoMessage.success
-      )
-    }
-    if(auth){
-      this.notificacion.mensaje(
-        'Usuario',
-        'Acceso denegado',
-        TipoMessage.warning
-      )
-    }
-   })
+    //Mensaje usuario registrado
+    let register = false;
+    let auth = "";
+    //Obtener parámetros de la URL
+    this.route.queryParams.subscribe((params) => {
+      register = params["register"] === "true" || false;
+      auth = params["auth"] || "";
+      if (register) {
+        this.notificacion.mensaje(
+          "Usuario",
+          "Uusario registrado! Especifique sus credenciales",
+          TipoMessage.success
+        );
+      }
+      if (auth) {
+        this.notificacion.mensaje(
+          "Usuario",
+          "Acceso denegado",
+          TipoMessage.warning
+        );
+        console.log(register);
+      }
+    });
   }
   onReset() {
     this.formulario.reset();
   }
   submitForm() {
-    this.makeSubmit=true;
+    this.makeSubmit = true;
     //Validación
-    if(this.formulario.invalid){
-     return;
+    if (this.formulario.invalid) {
+      return;
     }
-    this.authService.loginUser(this.formulario.value)
-    .subscribe((respuesta:any)=>{
-     this.router.navigate(['/']);
-    })
+    this.authService
+      .loginUser(this.formulario.value)
+      .subscribe((respuesta: any) => {
+        this.router.navigate(["/"]);
+      });
   }
   /* Manejar errores de formulario en Angular */
 
