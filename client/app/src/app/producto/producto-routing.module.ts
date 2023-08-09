c32fffd9ddf67757174c346204e34e8cbe06dcea
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-08 21:31:18 -0600

    Update producto-routing.module.ts

--- client/app/src/app/producto/producto-routing.module.ts
+++ client/app/src/app/producto/producto-routing.module.ts
@@ -5,12 +5,13 @@ import { ProductoAllComponent } from './producto-all/producto-all.component';
 import { ProductoDetailComponent } from './producto-detail/producto-detail.component';
 import { ProductoFormComponent } from './producto-form/producto-form.component';
 import { ProductoIndexComponent } from './producto-index/producto-index.component';
+import { AuthGuard } from '../share/guards/auth.guard';
 
 const routes: Routes = [
 {path: 'producto', component: ProductoIndexComponent},
-{path: 'producto/all', component: ProductoAllComponent},
-{path: 'producto/create', component: ProductoFormComponent},
-{path: 'producto/:id', component: ProductoDetailComponent},
+{path: 'producto/all' , canActivate:[AuthGuard], data:{roles:[2],}, component: ProductoAllComponent},
+{path: 'producto/create', canActivate:[AuthGuard], data:{roles:[2],}, component: ProductoFormComponent},
+{path: 'producto/:id', canActivate:[AuthGuard], data:{roles:[2],},component: ProductoDetailComponent},
 {path: 'producto/update/:id', component: ProductoFormComponent}
 ];
 
