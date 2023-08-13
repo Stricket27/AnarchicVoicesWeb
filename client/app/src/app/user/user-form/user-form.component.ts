29136e22e8818023d46adfe24886c51402c75550
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-12 23:01:07 -0600

    Update user-form.component.ts

--- client/app/src/app/user/user-form/user-form.component.ts
+++ client/app/src/app/user/user-form/user-form.component.ts
@@ -1,4 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from "@angular/forms";
+import { ActivatedRoute, Params, Router } from "@angular/router";
+import { Subject, takeUntil } from "rxjs";
+import { CurrencyPipe } from "@angular/common";
+import { GenericService } from "src/app/share/generic.service";
+import { DomSanitizer } from "@angular/platform-browser";
+import { NotificacionService } from "src/app/share/notification.service";
+import { AuthenticationService } from "src/app/share/authentication.service";
 
 @Component({
   selector: 'app-user-form',
@@ -6,5 +14,121 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-form.component.css']
 })
 export class UserFormComponent {
+  destroy$: Subject<boolean> = new Subject<boolean>();
+  userInfo: any;
+  respUser: any;
+  submitted = false;
+  userForm: FormGroup;
+  idUsuario: number = 0;
+  isCreate: boolean = true;
+
+
+constructor(
+    private fb: FormBuilder,
+    private gService: GenericService,
+    private router: Router,
+    private activeRouter: ActivatedRoute,
+    private noti: NotificacionService,
+    private authService: AuthenticationService,
+    private sanitizer: DomSanitizer
+  ) {
+    this.formularioReactive();
+  }
+
+  
+
+  ngOnInit(): void{
+    this.activeRouter.params.subscribe((params:Params) =>{
+      this.idUsuario = params['id_usuario'];
+      if(this.idUsuario != undefined){
+        this.isCreate = false;
+
+        this.gService.get('usuario', this.idUsuario).pipe(takeUntil(this.destroy$))
+        .subscribe((data: any) =>{
+          this.userInfo = data;
+          console.log(this.userInfo);
+
+          this.userForm.setValue({
+            id_usuario: this.userInfo.id_usuario,
+            nombre: this.userInfo.nombre,
+            apellidos: this.userInfo.apellidos,
+            numero_telefono: this.userInfo.numero_telefono,
+            correo_electronico: this.userInfo.correo_electronico,
+            contrasenna: this.userInfo.contrasenna,
+            estado_actual: this.userInfo.estado_actual
+          });
+        });
+      }
+    });
+  }
+
+  formularioReactive(){
+    this.userForm = this.fb.group({
+      id_usuario: [null,null],
+      nombre: [''],
+      apellidos: [''],
+      numero_telefono: [''],
+      correo_electronico: [''],
+      contrasenna: [''],
+      estado_actual: ['']
+    })
+  }
+
+  crearUsuario(): void{
+    this.submitted = true;
+
+    if(this.userForm.invalid){
+      return;
+    }
+
+    this.gService.create('usuario', this.userForm.value)
+    .pipe(takeUntil(this.destroy$)).subscribe((data: any) => {
+      this.respUser = data;
+      this.router.navigate(['/user/all'], {
+        queryParams: {create: 'true'}
+      });
+    });
+
+  }
+
+  actualizarUsuario(){
+    console.log(this.userForm.value);
+    this.gService
+    .update("usuario", this.userForm.value)
+    .pipe(takeUntil(this.destroy$))
+    .subscribe((data:any) => {
+      this.router.navigate(["/user.all"], {
+        queryParams: { update: true},
+      });
+    });
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.userForm.reset();
+  }
+  onBack() {
+    this.router.navigate(['/user/all']);
+  }
+  ngOnDestroy() {
+    this.destroy$.next(true);
+    // Desinscribirse
+    this.destroy$.unsubscribe();
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
