150925001fb0c19985bb8a671994dd7aa66c7035
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-14 01:47:39 -0600

    Update direccion.module.ts

--- client/app/src/app/direccion/direccion.module.ts
+++ client/app/src/app/direccion/direccion.module.ts
@@ -6,6 +6,25 @@ import { DireccionIndexComponent } from './direccion-index/direccion-index.compo
 import { DireccionFormComponent } from './direccion-form/direccion-form.component';
 import { DireccionDiagComponent } from './direccion-diag/direccion-diag.component';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatDividerModule} from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';  
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule} from "@angular/material/dialog";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CurrencyPipe } from '@angular/common';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [
@@ -16,7 +35,26 @@ import { DireccionDiagComponent } from './direccion-diag/direccion-diag.componen
   ],
   imports: [
     CommonModule,
-    DireccionRoutingModule
+    DireccionRoutingModule,
+
+    
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    NgxDropzoneModule,
+    CarouselModule,
+    MatChipsModule
   ]
 })
 export class DireccionModule { }
