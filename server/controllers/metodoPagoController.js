eacc734b77ef8aec4536e6ca8edb0331e6d3d370
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-14 12:45:42 -0600

    Update metodoPagoController.js

--- server/controllers/metodoPagoController.js
+++ server/controllers/metodoPagoController.js
@@ -1,63 +1,70 @@
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
-//Obtener listado
-module.exports.get = async (request, response, next) => {
-  const mp = await prisma.metodoPago.findMany({
-   
-  
-  });
-  response.json(mp);
-};
-//Obtener por Id
-//locahost:3000/usuario/2
-module.exports.getById = async (request, response, next) => {
-    let id=parseInt(request.params.id);
-    const mp = await prisma.metodoPago.findUnique({
-      where: { id_metodoPago: id },
-      include: {
-        usuario: {
-          include: {
-            tipoPago: true
-          }
-        }
-      }
-    });
-    response.json(mp);
-};
-//Crear 
-module.exports.create = async (request, response, next) => {
-  let metodoP = request.body;
-  const newMetodoP = await prisma.metodoPago.create({
-    data: {
-      numeroCuenta: metodoP.numeroCuenta,
-      fechaExpiracion: metodoP.fechaExpiracion,
-      estado_actual: "Activo",
-      id_usuario: metodoP.id_usuario,
-      id_tipoPago: metodoP.id_tipoPago,
-    },
-  });
-  response.json(newMetodoP);
-};
-//Actualizar un usuario
-module.exports.update = async (request, response, next) => {
-  let metodoP = request.body;
-  let idMp = parseInt(request.params.id);
-  //Obtener usuario viejo
-  // const usuarioViejo = await prisma.usuario.findUnique({
-  //   where: { id: idUsuario }
-  // });
+const { PrismaClient } = require("@prisma/client");
+const { response } = require("express");
+const { request } = require("http");
 
-  const newMetodoP = await prisma.metodoPago.update({
-    where: {
-      id: idMp,
-    },
-    data: {
-      numeroCuenta: metodoP.numeroCuenta,
-      fechaExpiracion: metodoP.fechaExpiracion,
-      estado_actual: "Activo",
-      id_usuario: metodoP.id_usuario,
-      id_tipoPago: metodoP.id_tipoPago,
+const prisma = new PrismaClient();
+module.exports.get = async (request, response, next) => {
+  const metodoPago = await prisma.metodoPago.findMany({
+    orderBy: {
+      numeroCuenta: "asc",
     },
   });
-  response.json(newMetodoP);
+  response.json(metodoPago);
+};
+
+module.exports.getById = async (request, response, next) => {
+  let id = parseInt(request.params.id);
+  const metodoPago = await prisma.metodoPago.findUnique({
+    where: { id_metodoPago: id },
+    include: {
+      usuario: {
+        select: {
+          id_usuario: true,
+        },
+      },
+      tipoPago: {
+        select: {
+          id_tipoPago: true,
+        },
+      },
+    },
+  });
+  response.json(metodoPago);
+};
+
+module.exports.create = async (request, response, next) => {
+  let metodoPago = request.body;
+
+  const newMetodoPago = await prisma.metodoPago.create({
+    data: {
+      numeroCuenta: metodoPago.numeroCuenta,
+      mesVencimiento: metodoPago.mesVencimiento,
+      annoVencimiento: metodoPago.annoVencimiento,
+      codigoSeguridad: metodoPago.codigoSeguridad,
+      estado_actual: metodoPago.estado_actual,
+      usuario: { connect: { id_usuario: metodoPago.usuario } },
+      tipoPago: { connect: { id_tipoPago: metodoPago.tipoPago } },
+    },
+  });
+  response.json(newMetodoPago);
+};
+
+module.exports.update = async (request, response, next) => {
+  let metodoPago = request.body;
+  const { id_metodoPago } = request.body;
+  console.log(request.body);
+
+  const newMetodoPago = await prisma.metodoPago.update({
+    where: {
+      id_metodoPago: id_metodoPago,
+    },
+    numeroCuenta: metodoPago.numeroCuenta,
+    mesVencimiento: metodoPago.mesVencimiento,
+    annoVencimiento: metodoPago.annoVencimiento,
+    codigoSeguridad: metodoPago.codigoSeguridad,
+    estado_actual: metodoPago.estado_actual,
+    usuario: { connect: { id_usuario: metodoPago.usuario } },
+    tipoPago: { connect: { id_tipoPago: metodoPago.id_tipoPago } },
+  });
+  response.json(newMetodoPago);
 };
