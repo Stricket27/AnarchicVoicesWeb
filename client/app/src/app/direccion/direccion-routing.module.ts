52b62668140f1ace620ef58a03abc7e682a163c3
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-14 01:48:04 -0600

    Update direccion-routing.module.ts

--- client/app/src/app/direccion/direccion-routing.module.ts
+++ client/app/src/app/direccion/direccion-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { DireccionFormComponent } from './direccion-form/direccion-form.component';
+import { DireccionIndexComponent } from './direccion-index/direccion-index.component';
+
+const routes: Routes = [
+
+{path: 'direccion', component: DireccionIndexComponent},
+{path: 'direccion/create', component: DireccionFormComponent},
+{path: 'direcion/update/id', component: DireccionFormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
