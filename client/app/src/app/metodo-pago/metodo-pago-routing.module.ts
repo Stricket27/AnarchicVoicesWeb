d4fa31e0a00dea9b6d81a0abf7edd0fc4280d4c7
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-14 12:47:37 -0600

    Update metodo-pago-routing.module.ts

--- client/app/src/app/metodo-pago/metodo-pago-routing.module.ts
+++ client/app/src/app/metodo-pago/metodo-pago-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { MetodoPagoFormComponent } from './metodo-pago-form/metodo-pago-form.component';
+import { MetodoPagoIndexComponent } from './metodo-pago-index/metodo-pago-index.component';
+
+const routes: Routes = [
+
+  {path: 'metodoPago', component: MetodoPagoIndexComponent},
+  {path: 'metodoPago/create', component: MetodoPagoFormComponent},
+  {path: 'metodoPago/update/:id', component: MetodoPagoFormComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
