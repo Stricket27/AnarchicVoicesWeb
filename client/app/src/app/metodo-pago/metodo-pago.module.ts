5f0e05f363b35bed260ff62f864d1e8901bb6202
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-14 12:47:27 -0600

    Update metodo-pago.module.ts

--- client/app/src/app/metodo-pago/metodo-pago.module.ts
+++ client/app/src/app/metodo-pago/metodo-pago.module.ts
@@ -7,6 +7,26 @@ import { MetodoPagoFormComponent } from './metodo-pago-form/metodo-pago-form.com
 import { MetodoPagoIndexComponent } from './metodo-pago-index/metodo-pago-index.component';
 import { MetodoPagoDiagComponent } from './metodo-pago-diag/metodo-pago-diag.component';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatDividerModule} from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';  
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule} from "@angular/material/dialog";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CurrencyPipe } from '@angular/common';
+import { NgxDropzoneModule } from 'ngx-dropzone';
+
+import { CarouselModule } from 'ngx-bootstrap/carousel';
+import { MatChipsModule} from '@angular/material/chips';
+
 
 @NgModule({
   declarations: [
@@ -17,7 +37,25 @@ import { MetodoPagoDiagComponent } from './metodo-pago-diag/metodo-pago-diag.com
   ],
   imports: [
     CommonModule,
-    MetodoPagoRoutingModule
+    MetodoPagoRoutingModule,
+
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    NgxDropzoneModule,
+    CarouselModule,
+    MatChipsModule
   ]
 })
 export class MetodoPagoModule { }
