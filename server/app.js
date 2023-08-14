7eb3c4c39b7d75c5de0a965d31e5cd968138e14b
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-14 12:39:09 -0600

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const logger = require("morgan");
 const app = express();
 const prism = new PrismaClient();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
 //---Archivos de rutas---
 const userRoutes = require("./routes/userRoutes");
@@ -15,11 +15,12 @@ const calificacionUsuarioRoutes = require("./routes/calificacionUsuarioRoutes");
 const direccionRoutes = require("./routes/direccionRoutes");
 const ordenCompraRoutes = require("./routes/ordenCompraRoutes");
 const productoRoutes = require("./routes/productoRoutes");
-const fotografiaRoutes = require("./routes/fotografiaRoutes")
+const fotografiaRoutes = require("./routes/fotografiaRoutes");
 const categoriaRoutes = require("./routes/categoriaRoutes");
 const metodoPagoRoutes = require("./routes/metodoPagoRoutes");
+const tipoPagoRoutes = require("./routes/tipoPagoRoutes");
 const mensajeriaRoutes = require("./routes/mensajeriaRoutes");
-const detalle_usuarioTipo = require("./routes/detalle_usuarioTipoRoutes")
+const detalle_usuarioTipo = require("./routes/detalle_usuarioTipoRoutes");
 
 // Acceder a la configuracion del archivo .env
 dotEnv.config();
@@ -32,16 +33,15 @@ app.use(logger("dev"));
 // Middleware para gestionar Requests y Response json
 app.use(express.json());
 app.use(
-express.urlencoded({
-extended: true,
-})
+  express.urlencoded({
+    extended: true,
+  })
 );
 
+app.use(express.static("uploads"));
 
-app.use(express.static('uploads'));
-
-app.use(bodyParser.json({ limit: '100mb' }));
-app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(bodyParser.json({ limit: "100mb" }));
+app.use(bodyParser.urlencoded({ limit: "100mb", extended: true }));
 
 //---- Definir rutas ----
 app.use("/user/", userRoutes);
@@ -49,15 +49,16 @@ app.use("/tipoUsuario/", tipoUsuarioRoutes);
 app.use("/calificacionUsuario/", calificacionUsuarioRoutes);
 app.use("/direccion/", direccionRoutes);
 app.use("/ordenCompra/", ordenCompraRoutes);
-app.use("/ordenCompraV/",  ordenCompraRoutes);
+app.use("/ordenCompraV/", ordenCompraRoutes);
 app.use("/producto/", productoRoutes);
 app.use("/fotografia/", fotografiaRoutes);
 app.use("/categoria/", categoriaRoutes);
 app.use("/metodoPago/", metodoPagoRoutes);
-app.use("/mensajeria/",mensajeriaRoutes);
+app.use("/tipoPago/", tipoPagoRoutes);
+app.use("/mensajeria/", mensajeriaRoutes);
 app.use("/detalle_usuarioTipo/", detalle_usuarioTipo);
 // Servidor
-app.listen(port, () => { 
-console.log(`http://localhost:${port}`);
-console.log("Presione CTRL-C para deternerlo\n");
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+  console.log("Presione CTRL-C para deternerlo\n");
 });
