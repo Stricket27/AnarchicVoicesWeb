437a29a1663ce2506bdcfe8361786136269935e5
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-22 22:42:59 -0600

    Update calificacion-usuario.module.ts

--- client/app/src/app/calificacion-usuario/calificacion-usuario.module.ts
+++ client/app/src/app/calificacion-usuario/calificacion-usuario.module.ts
@@ -5,13 +5,17 @@ import { CalificacionUsuarioRoutingModule } from './calificacion-usuario-routing
 import { CalificacionUsuarioAllComponent } from './calificacion-usuario-all/calificacion-usuario-all.component';
 import { CalificacionUsuarioIndexComponent } from './calificacion-usuario-index/calificacion-usuario-index.component';
 import { CalificacionUsuarioFormComponent } from './calificacion-usuario-form/calificacion-usuario-form.component';
+import { CalificacionUsuarioTop3Component } from './calificacion-usuario-top3/calificacion-usuario-top3.component';
+import { CalificacionUsuarioTop5Component } from './calificacion-usuario-top5/calificacion-usuario-top5.component';
 
 
 @NgModule({
   declarations: [
     CalificacionUsuarioAllComponent,
     CalificacionUsuarioIndexComponent,
-    CalificacionUsuarioFormComponent
+    CalificacionUsuarioFormComponent,
+    CalificacionUsuarioTop3Component,
+    CalificacionUsuarioTop5Component
   ],
   imports: [
     CommonModule,
