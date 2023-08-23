02e8a48e0f7183e30baeaadd8b4e39110b34b157
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-22 22:02:41 -0600

    Update ordenCompraRoutes.js

--- server/routes/ordenCompraRoutes.js
+++ server/routes/ordenCompraRoutes.js
@@ -11,12 +11,12 @@ router.get('/', ordenCompraController.get);
 
 router.post('/', ordenCompraController.create);
 
+router.put('/', ordenCompraController.update);
+
 router.get("/vProductoTop", ordenCompraController.getVentaProductoTop);
 
 router.get("/vProducto/:mes", ordenCompraController.getVentaProductoMes);
 
-router.get('/:id', /*auth.grantRole(["Administrador", "Vendedor"]),*/ ordenCompraController.getById);
-
-router.put('/:id', ordenCompraController.update);
+router.get('/:id', ordenCompraController.getById);
 
 module.exports=router
