7970faa6810fd92891f6dbd18c45c260d4f0fbb8
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-22 23:02:55 -0600

    Update calificacion-usuario.module.ts

--- client/app/src/app/calificacion-usuario/calificacion-usuario.module.ts
+++ client/app/src/app/calificacion-usuario/calificacion-usuario.module.ts
@@ -8,6 +8,22 @@ import { CalificacionUsuarioFormComponent } from './calificacion-usuario-form/ca
 import { CalificacionUsuarioTop3Component } from './calificacion-usuario-top3/calificacion-usuario-top3.component';
 import { CalificacionUsuarioTop5Component } from './calificacion-usuario-top5/calificacion-usuario-top5.component';
 
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card'; 
+import { MatIconModule } from '@angular/material/icon'; 
+import { MatButtonModule } from '@angular/material/button'; 
+import { MatDividerModule} from '@angular/material/divider';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';  
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule} from "@angular/material/dialog";
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+
 
 @NgModule({
   declarations: [
@@ -19,7 +35,23 @@ import { CalificacionUsuarioTop5Component } from './calificacion-usuario-top5/ca
   ],
   imports: [
     CommonModule,
-    CalificacionUsuarioRoutingModule
+    CalificacionUsuarioRoutingModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatDividerModule,
+    MatDialogModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatDatepickerModule,
+    FormsModule, 
+    ReactiveFormsModule,
   ]
 })
 export class CalificacionUsuarioModule { }
