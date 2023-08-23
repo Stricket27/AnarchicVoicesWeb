cf9ce08d5360089ca4760a11384a26fa2ee9a6f1
Stricket27 <57728628+Stricket27@users.noreply.github.com>
2023-08-22 22:43:43 -0600

    Update calificacion-usuario-routing.module.ts

--- client/app/src/app/calificacion-usuario/calificacion-usuario-routing.module.ts
+++ client/app/src/app/calificacion-usuario/calificacion-usuario-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CalificacionUsuarioTop3Component } from './calificacion-usuario-top3/calificacion-usuario-top3.component';
+import { CalificacionUsuarioTop5Component } from './calificacion-usuario-top5/calificacion-usuario-top5.component';
+
+const routes: Routes = [
+
+   {path: 'calificacionUsuarioTop3/rPDF', component: CalificacionUsuarioTop3Component},
+  {path: 'calificacionUsuarioTop5/rPDF', component: CalificacionUsuarioTop5Component}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
